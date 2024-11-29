1a5c991bf9a66cfc58076678c29f038180db91ab
PhillipBraendle <phillip.braendle@gmx.de>
2024-11-29 11:44:37 +0100

    implement get node

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,19 @@ const express = require('express');
 const app = express();
 const port = 2000;
 const server = require('http').createServer(app);
+const fs = require('fs');
+const path = require('path');
 
 app.get('/todos', (req, res) => {
-    res.json({});
+    const todosPath = path.join(__dirname, 'todos.json');
+
+    fs.readFile(todosPath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
 server.listen(port, () => {
